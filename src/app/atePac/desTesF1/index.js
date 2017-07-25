92fca7f423b9ebaacb7e91e09b8164f250b0034b
André Lorenz <andre.lorenz@philips.com>
2017-07-25 12:41:16 -0300

    Create index.js file

---
+++ src/app/atePac/desTesF1/index.js
@@ -0,0 +1,10 @@
+import app from "../";
+import _module from "../_module";
+import * as components from "./components/";
+const feature = app.feature({
+  action: "destesf1",
+  code: 4806,
+  function: 9906,
+  module: _module
+}).addAll(components).register();
+export default feature;
